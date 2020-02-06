471d8800884f760314ac64a1a8244eaa7b5f122d
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-02-06 19:46:34 +0900

    user sign up activate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'todo/index'
-  get 'todo/new'
-  get 'todo/edit'
+  root 'users#index'
+  get   '/signup', to: 'users#new'
+  resources :users, except: [:new]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
