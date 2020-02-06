3be36c0d54db06764bb217605599a0d99ee6ae98
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-02-06 17:35:38 +0900

    secure config setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
+require 'digest/md5'
 class ApplicationController < ActionController::Base
+
+    # digest_auth
+    REALM = 'SecretZone'.freeze
+    USERS = {'user1' => Digest:MD5.hexdigest(['user1', REALM, 'password'].join(':'))}.freeze
+    before_action :authenticate
+
+    # CSRF
+    protect_from_forgery with: :exception
+
+    private
+        # digest_auth
+        def authenticate
+            authenticate_or/request/with_http_deigest(REALM) do |username|
+                USERS[username]
+            end
+        end
 end
