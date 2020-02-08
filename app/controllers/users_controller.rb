95b0ac0b675962ef01a56d623a4f90c6a0bf6b8e
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-02-08 16:24:05 +0900

    set up basic_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to this App!"
       redirect_to @user
     else
