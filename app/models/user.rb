c322b294c2f0225e950bec707f195e205a38b573
zenos <zenos.game.anime.all.vo1@gmail.com>
2020-02-13 21:17:41 +0900

    user relation setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     has_many :todos, dependent: :destroy
+
+    has_many :relationships
+    has_many :followings, through: :relationships, source: :follow
+    has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+    has_many :followers, through: :reverse_of_relationships, source: :user
+
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
@@ -11,6 +17,23 @@ class User < ApplicationRecord
     has_secure_password # tutorial 6.3章
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+    # -------- Relationship --------
+    def follow(other_user)
+      unless self == other_user
+        self.relationships.find_or_create_by(follow_id: other_user.id)
+      end
+    end
+  
+    def unfollow(other_user)
+      relationship = self.relationships.find_by(follow_id: other_user.id)
+      relationship.destroy if relationship
+    end
+  
+    def following?(other_user)
+      self.followings.include?(other_user)
+    end
+
+    # -------- 認証 --------
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get   '/signup', to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get     '/signup', to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
   resources :users
   resources :todos
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
